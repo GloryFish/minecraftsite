1f5e7a95eba714710e8042d5475f3dc6a7c95955
Jay Roberts <jay@gloryfish.org>
2010-08-26 13:45:33 -0400

    Add symlinks for other map types

--- scripts/maps.sh
+++ scripts/maps.sh
@@ -11,4 +11,7 @@ OBAFILE=gfworld-oba-`date +%Y%m%d-%H%M`.png
 ~/git/pynemap/pynemap.py -o $OBFILE -r oblique -v ~/minecraft/gfworld/level.dat 
 ~/git/pynemap/pynemap.py -o $OBAFILE -r oblique_angled -v ~/minecraft/gfworld/level.dat 
 
+ln -s $TOPFILE overhead.png
+ln -s $OBFILE oblique.png
+ln -s $OBAFILE oblique-angled.png
 ln -s $OBFILE current.png
