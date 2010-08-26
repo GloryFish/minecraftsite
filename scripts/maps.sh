b86602505be536662df515fcf0f2cc717407ba37
Jay Roberts <jay@gloryfish.org>
2010-08-25 20:01:47 -0400

    Update maps script

--- scripts/maps.sh
+++ scripts/maps.sh
@@ -3,10 +3,12 @@
 # make catographer maps
 cd ~/git/minecraftsite.git
 
-THEFILE=gfworld-`date +%Y%m%d-%H%M`.png
+OBFILE=gfworld-ob-`date +%Y%m%d-%H%M`.png
+TOPFILE=gfworld-top-`date +%Y%m%d-%H%M`.png
+OBAFILE=gfworld-oba-`date +%Y%m%d-%H%M`.png
 
-~/git/pynemap/pynemap.py -o $THEFILE
+~/git/pynemap/pynemap.py -o $TOPFILE -r overhead -v ~/minecraft/gfworld/level.dat 
+~/git/pynemap/pynemap.py -o $OBFILE -r oblique -v ~/minecraft/gfworld/level.dat 
+~/git/pynemap/pynemap.py -o $OBAFILE -r oblique_angled -v ~/minecraft/gfworld/level.dat 
 
-sleep 1
-
-ln $THEFILE current.png
+ln -s $OBFILE current.png
