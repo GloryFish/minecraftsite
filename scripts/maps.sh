ae0347197c07764fa323fcf0f4d03ca57193a6f0
gloryfish <gloryfish@jericho.(none)>
2010-09-13 03:47:32 +0000

    Update maps.sh to use ct10t

--- scripts/maps.sh
+++ scripts/maps.sh
@@ -3,20 +3,16 @@
 # make catographer maps
 cd ~/git/minecraftsite.git/www/maps/
 
-OBFILE=gfworld-ob-`date +%Y%m%d-%H%M`.png
 TOPFILE=gfworld-top-`date +%Y%m%d-%H%M`.png
 OBAFILE=gfworld-oba-`date +%Y%m%d-%H%M`.png
 
-~/git/pynemap/pynemap.py -o $TOPFILE -r overhead -v ~/minecraft/gfworld/level.dat 
-~/git/pynemap/pynemap.py -o $OBFILE -r oblique -v ~/minecraft/gfworld/level.dat 
-~/git/pynemap/pynemap.py -o $OBAFILE -r oblique_angled -v ~/minecraft/gfworld/level.dat 
+~/ct10t/ct10t -o $TOPFILE -w ~/minecraft/gfworld/
+~/ct10t/ct10t -o $OBAFILE -w ~/minecraft/gfworld/ -y  
 
 rm -f overhead.png
-rm -f oblique.png
 rm -f oblique-angled.png
 rm -f current.png
 
 ln $TOPFILE overhead.png
-ln $OBFILE oblique.png
 ln $OBAFILE oblique-angled.png
-ln $OBFILE current.png
+ln $OBAFILE current.png
