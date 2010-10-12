dbd79787e7fc05892188c912c3c2bc193fe70017
Jay Roberts <jay@gloryfish.org>
2010-10-11 21:51:52 -0400

    Don't delete old backups

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -3,6 +3,3 @@
 THEDATE=`date +%Y%m%d-%H%M`
 
 7z a ~/minecraft/backups/gfworld.$THEDATE.7z ~/minecraft/gfworld
-
-# remove old backups, anything over 5days old
-find ~/minecraft/backups -type f -mtime +5 -exec rm -f '{}' \;
