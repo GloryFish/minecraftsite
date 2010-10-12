cb594e4db446f6c8e1d80e1f8a79a0ba55b9d5da
Jay Roberts <gloryfish@jericho.gloryfish.org>
2010-10-12 02:00:59 +0000

    Update map script for new server

--- scripts/maps.sh
+++ scripts/maps.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 # make catographer maps
-cd ~/git/minecraftsite.git/www/maps/
+cd ~/git/minecraftsite/www/maps/
 
 TOPFILE=gfworld-top-`date +%Y%m%d-%H%M`.png
 OBAFILE=gfworld-oba-`date +%Y%m%d-%H%M`.png
 
-~/ct10t/ct10t -o $TOPFILE -w ~/minecraft/gfworld/
-~/ct10t/ct10t -o $OBAFILE -w ~/minecraft/gfworld/ -y  
+~/git/c10t/c10t -o $TOPFILE -w ~/minecraft/gfworld/
+~/git/c10t/c10t -o $OBAFILE -w ~/minecraft/gfworld/ -y  
 
 rm -f overhead.png
 rm -f oblique-angled.png
